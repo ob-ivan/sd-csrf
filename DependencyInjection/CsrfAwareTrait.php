43edb71f966b779e9d52162540dd675e39a6bbeb
Ivan Vegner <vegner@slrealty.ru>
2017-08-18 12:26:00 +0300

    Update aware trait to modern dependency injection standards

--- DependencyInjection/CsrfAwareTrait.php
+++ DependencyInjection/CsrfAwareTrait.php
@@ -4,9 +4,14 @@ namespace SD\Csrf\DependencyInjection;
 use SD\Csrf\Manager;
 
 trait CsrfAwareTrait {
+    private $autoDeclareCsrf = 'csrf';
     private $csrf;
 
     public function setCsrf(Manager $csrf) {
         $this->csrf = $csrf;
+    }
+
+    private function getCsrf(): Manager {
+        return $this->csrf;
     }
 }
